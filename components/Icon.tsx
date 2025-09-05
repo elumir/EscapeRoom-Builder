e013b5b5dbdfa145c03996634e272d50b4258c11
Errol Elumir <errol@elumir.com>
2025-09-04 21:43:02 -0400

    fix(icon): Update restart icon SVG
    The existing restart icon SVG was an outdated representation. This commit replaces it with a more accurate and visually distinct SVG for the restart functionality.

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -70,7 +70,8 @@ const icons = {
   ),
   restart: (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0 0 11.667 0m0 0l3.181-3.183m-3.181 3.183L16.023 9.348" />
+      <circle cx="12" cy="11.998" r="11.25" strokeLinecap="round" strokeLinejoin="round" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9.284 7.964A1.591 1.591 0 0 1 12 9.089V10.5l2.534-2.534a1.591 1.591 0 0 1 2.716 1.123v5.819a1.592 1.592 0 0 1-2.716 1.125L12 13.5v1.41a1.592 1.592 0 0 1-2.716 1.125L5.25 12z" />
     </svg>
   )
 };
